fae66fb6136c0753d8f011a0e163ea52ec1c528f
Shekhor-B <152231681+Shekhor-B@users.noreply.github.com>
2026-08-08 12:18:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -300,4 +300,4 @@ def predict_risk(req: ScenarioRequest):
     )
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="seismicinteractive.onrender.com", port=10000, reload=True)
